3fe8f6f837e7d1b04d3bb632702164a658486ed0
Ashwin <ashwinprayaga@gmail.com>
2022-05-14 16:16:32 -0500

    Added search bar component & cross-origin requests

---
+++ blindcs-frontend/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class SearchBar extends Component {
+  constructor() {
+    super()
+    this.state = { value: "" };
+  }
+
+  _handleKeyDown = (e) => {
+    // TODO: Update requests to correct endpoints
+    if (e.key == "Enter") {
+      axios
+        .get('http://localhost:8080/api/v1/account', {
+          searchText: e.target.value
+        })
+        .then(({ data }) => {
+          console.log(data);
+        });
+    }
+  }
+
+  _handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <h1>React Search</h1>
+        <div className="search">
+          <input type="text" placeholder="Search"
+            onKeyDown={this._handleKeyDown}
+            onChange={this._handleChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
--- blindcs/src/main/java/com/interviewprep/blindcs/BlindcsApplication.java
+++ blindcs/src/main/java/com/interviewprep/blindcs/BlindcsApplication.java
@@ -2,8 +2,11 @@ package com.interviewprep.blindcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -22,6 +25,16 @@ public class BlindcsApplication {
         System.out.println(t1.test());
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // Allows cross-origin http requests when developing on the local react frontend
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
 
 
